b4c70cae44d8c5c4fad57783b2c9aef93cb6f9b1
Mostafa Saad <36232255+mostafaemara@users.noreply.github.com>
2021-12-28 21:25:42 +0200

    add helpers for transaction item

--- lib/src/extenstions/category_helper.dart
+++ lib/src/extenstions/category_helper.dart
@@ -1,13 +1,17 @@
 import 'package:expense_tracker_app/src/bloc/categories/categories_cubit.dart';
 import 'package:expense_tracker_app/src/models/category.dart';
+import 'package:expense_tracker_app/src/models/multilingual.dart';
 import 'package:expense_tracker_app/src/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 extension CategoryListHelper on List<Category> {
   String categoryIconById(String id) {
-    print("--------------------------------------------------------" + id);
     return firstWhere((element) => element.id == id).iconUrl;
+  }
+
+  Multilingual categoryTitleById(String id) {
+    return firstWhere((element) => element.id == id).title;
   }
 
   Category categoryById(String id) {
@@ -18,15 +22,9 @@ extension CategoryListHelper on List<Category> {
 extension CategoriesHelper on BuildContext {
   List<Category> categories(TransactionType type) {
     if (type == TransactionType.expense) {
-      print("-------------------------------------------------------Expense");
       return read<CategoriesCubit>().state.expenseCategories;
     } else {
-      print("-------------------------------------------------------income");
       return read<CategoriesCubit>().state.incomeCategories;
     }
-  }
-
-  String iconUrlById(String id, TransactionType type) {
-    final categories = categories(type);
   }
 }
