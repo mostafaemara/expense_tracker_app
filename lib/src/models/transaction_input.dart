7fab0c142f52181e4a39f4057411fbce7b474009
Mostafa Saad <36232255+mostafaemara@users.noreply.github.com>
2022-01-02 21:49:31 +0200

    add work manger

--- lib/src/models/transaction_input.dart
+++ lib/src/models/transaction_input.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart' as firebase;
 import 'package:expense_tracker_app/src/models/category.dart';
+import 'package:expense_tracker_app/src/models/periodic_type.dart';
 import 'package:expense_tracker_app/src/models/transaction.dart';
 
 class TransactionInput {
@@ -8,16 +10,37 @@ class TransactionInput {
   final String description;
   final String attachment;
   final String accountId;
+  final bool repeat;
+  late final DateTime? endDate;
+  late final PeriodicType? periodicType;
+
   TransactionInput(
       {required this.accountId,
+      this.endDate,
+      this.repeat = false,
       required this.type,
       required this.category,
       required this.amount,
       required this.description,
       required this.attachment});
 
-  Map<String, dynamic> toMap() {
+  Map<String, dynamic> toTransactionMap() {
     return {
+      "type": type.toMap(),
+      "category": category.id,
+      "accountId": accountId,
+      "amount": amount,
+      "description": description,
+      "attachment": attachment,
+    };
+  }
+
+  Map<String, dynamic> toFrequentTransactionMap() {
+    final _endDateTimeStamp = firebase.Timestamp.fromDate(endDate!);
+
+    return {
+      "periodicType": periodicType!.toMap(),
+      "endDate": _endDateTimeStamp,
       "type": type.toMap(),
       "category": category.id,
       "accountId": accountId,
