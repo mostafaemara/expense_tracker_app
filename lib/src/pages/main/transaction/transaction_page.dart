db13e36bcdfc637d19d9e91d7b39eb9294c87882
Mostafa Saad <36232255+mostafaemara@users.noreply.github.com>
2022-01-30 16:18:17 +0200

    transaction_page added

--- lib/src/pages/main/transaction/transaction_page.dart
+++ lib/src/pages/main/transaction/transaction_page.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/filter_button.dart';
+import 'widgets/financial_report_button.dart';
+import 'widgets/section_title.dart';
+import 'widgets/transactions_list_view.dart';
+
 class TransactionPage extends StatelessWidget {
   const TransactionPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Transaction"),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: AppBar(
+                actions: [FilterButton()],
+              ),
+            ),
+            const FinancialReportButton(),
+            const SectionTitle(
+              title: "Today",
+            ),
+            const TransactionListView(
+              transactions: [],
+            ),
+            const SectionTitle(
+              title: "Yesterday",
+            ),
+            const TransactionListView(
+              transactions: [],
+            )
+          ],
+        ),
       ),
     );
   }
