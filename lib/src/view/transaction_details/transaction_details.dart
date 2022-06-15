46e388cc654fc010569adf7f7ba64ce7e3a65253
mostafa <mostafa.saad.emara@gmail.com>
2022-06-16 01:13:40 +0200

    finish budgets ui

--- lib/src/view/transaction_details/transaction_details.dart
+++ lib/src/view/transaction_details/transaction_details.dart
@@ -2,6 +2,7 @@ import 'package:expense_tracker_app/src/data/models/transaction.dart';
 import 'package:expense_tracker_app/src/helpers/transaction_helper.dart';
 
 import 'package:expense_tracker_app/src/styles/app_colors.dart';
+import 'package:expense_tracker_app/src/view/common/remove_button.dart';
 import 'package:flutter/material.dart';
 import '../../helpers/transaction_helper.dart';
 
@@ -29,7 +30,14 @@ class TransactionDetailsPage extends StatelessWidget {
               .titleTextStyle!
               .copyWith(color: Theme.of(context).colorScheme.onPrimary),
         ),
-        actions: const [DeleteButton()],
+        actions: [
+          DeleteButton(
+            color: AppColors.light,
+            body: "Are you sure do you wanna remove this transaction?",
+            title: "Remove this transaction?",
+            onDeleteConfirmed: () {},
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Stack(
@@ -98,84 +106,6 @@ class TransactionDetailsPage extends StatelessWidget {
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class DeleteButton extends StatelessWidget {
-  const DeleteButton({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-        onPressed: () => _showRemoveBottomSheet(context),
-        icon: Image.asset("assets/images/trash.png"));
-  }
-
-  void _showRemoveBottomSheet(BuildContext context) {
-    showBottomSheet(
-      context: context,
-      builder: (context) => SizedBox(
-        height: 217,
-        width: double.infinity,
-        child: Card(
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(24),
-                  bottomLeft: Radius.circular(24))),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Remove this transaction?",
-                  style: Theme.of(context).textTheme.subtitle1?.copyWith(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                      ),
-                ),
-                Text(
-                  "Are you sure do you wanna remove this transaction?",
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.subtitle1?.copyWith(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                      color: AppColors.light20),
-                ),
-                SizedBox(
-                  height: 56,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                          child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  onPrimary: AppColors.violet,
-                                  primary: AppColors.violet20),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: const Text("No"))),
-                      const SizedBox(
-                        width: 16,
-                      ),
-                      Expanded(
-                          child: ElevatedButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: const Text("Yes")))
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
         ),
       ),
     );
