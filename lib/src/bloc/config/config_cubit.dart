b79780fb43e1004b3289cd0b07b8d0b69e8cebda
mostafa <mostafa.saad.emara@gmail.com>
2022-06-20 13:38:22 +0200

    currency add

--- lib/src/bloc/config/config_cubit.dart
+++ lib/src/bloc/config/config_cubit.dart
@@ -1,1 +1,31 @@
+import 'package:expense_tracker_app/injection.dart';
+import 'package:expense_tracker_app/src/data/models/currency.dart';
+import 'package:expense_tracker_app/src/data/models/language.dart';
+import 'package:expense_tracker_app/src/data/repositories/config_repository.dart';
 
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'config_state.dart';
+
+class ConfigCubit extends Cubit<ConfigState> {
+  ConfigCubit() : super(ConfigState.init());
+  final _configRepo = locator<ConfigRepository>();
+  void init() async {
+    final themeMode = await _configRepo.readThemeMode();
+    final language = await _configRepo.readLanguageCode();
+    final currency = await _configRepo.readCurrency();
+
+    emit(state.copyWith(
+        isLoading: false,
+        currency: currency ?? Currency.usd,
+        language: language ?? Language.english,
+        themeMode: themeMode ?? ThemeMode.system));
+  }
+
+  void selectCurrency(Currency currency) async {
+    await _configRepo.writeCurrency(currency);
+    emit(state.copyWith(currency: currency));
+  }
+}
--- lib/src/bloc/config/config_state.dart
+++ lib/src/bloc/config/config_state.dart
@@ -1,1 +1,28 @@
+import 'package:expense_tracker_app/src/data/models/currency.dart';
+import 'package:flutter/material.dart';
 
+import 'package:expense_tracker_app/src/data/models/language.dart';
+
+class ConfigState {
+  final bool isLoading;
+  final Currency currency;
+  final Language language;
+  final ThemeMode themeMode;
+
+  const ConfigState(
+      this.currency, this.language, this.themeMode, this.isLoading);
+  ConfigState.init()
+      : currency = Currency.usd,
+        isLoading = true,
+        language = Language.english,
+        themeMode = ThemeMode.system;
+  ConfigState copyWith({
+    Currency? currency,
+    Language? language,
+    ThemeMode? themeMode,
+    bool? isLoading,
+  }) {
+    return ConfigState(currency ?? this.currency, language ?? this.language,
+        themeMode ?? this.themeMode, isLoading ?? this.isLoading);
+  }
+}
