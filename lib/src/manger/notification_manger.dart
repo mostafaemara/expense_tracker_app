3006581fd2aaf7f16b5fa45a941b990cd2344309
mostafa <mostafa.saad.emara@gmail.com>
2022-07-30 22:10:53 +0200

    add delete transaction

---
+++ lib/src/manger/notification_manger.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationManger {
+  final Function(String?) onSelectNotification;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+
+  NotificationManger(this.onSelectNotification);
+  Future<void> init() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+    final IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+      onDidReceiveLocalNotification: (id, title, body, payload) {},
+    );
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+    );
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+  }
+
+  Future<void> showNotification(int id, String title, String body,
+      {String? payload}) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+            'expense-tracker-app-general', 'expense tracker app general',
+            channelDescription: 'expense tracker app general',
+            importance: Importance.max,
+            priority: Priority.high,
+            ticker: 'ticker');
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin
+        .show(id, title, body, platformChannelSpecifics, payload: payload);
+  }
+}
