994274d2b381258e8703207307d7bb1c980704b2
Mostafa <mostafa.saad.emara@gmail.com>
2022-11-06 19:23:18 +0200

    adding frequency

--- lib/src/view/new_transaction/widgets/frequency_form_field.dart
+++ lib/src/view/new_transaction/widgets/frequency_form_field.dart
@@ -2,31 +2,28 @@ import 'package:expense_tracker_app/src/data/models/frequency.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class AccountFormField extends StatelessWidget {
-  const AccountFormField({
+class FrequencyFormField extends StatelessWidget {
+  const FrequencyFormField({
     Key? key,
     this.onChanged,
     this.selectedFrequency,
-    required this.frequencies,
-    this.targetAccountId,
   }) : super(key: key);
   final Function(Frequency?)? onChanged;
   final Frequency? selectedFrequency;
-  final List<Frequency> frequencies;
-  final String? targetAccountId;
+
   @override
   Widget build(BuildContext context) {
     return DropdownButtonFormField<Frequency>(
       validator: (v) =>
           v == null ? AppLocalizations.of(context)?.accountRequired : null,
       onChanged: onChanged,
-      hint: Text(AppLocalizations.of(context)!.accountName),
+      hint: Text(AppLocalizations.of(context)!.frequency),
       value: selectedFrequency,
       items: List.generate(
-          frequencies.length,
+          Frequency.values.length,
           (index) => DropdownMenuItem(
-                child: Text(frequencies[index].toString()),
-                value: frequencies[index],
+                child: Text(Frequency.values[index].toString()),
+                value: Frequency.values[index],
               )),
     );
   }
